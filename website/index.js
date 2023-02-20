f5d5de9fdfdb75ecee245030646fecd9a51c833c
Huseyincevik1 <96417296+Huseyincevik1@users.noreply.github.com>
2023-02-20 16:22:41 +0300

    web site
    basit web site yapımı

---
+++ website/index.js
@@ -0,0 +1,20 @@
+
+
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/users");
+
+app.set("view engine","ejs");
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+
+app.use(userRoutes);
+
+
+app.listen(3000,() => {
+   console.log("Node.js server at port 3000"); 
+   });
+
---
+++ website/routes/users.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const router = express.Router();
+const db = require("../data/db");
+
+router.use("/products/:id",async function(req,res)  {
+    try {
+       const [product,] = await  db.execute("select * from products where id=?",[req.params.id]);
+       res.render("product-details",{
+          urun: product[0]
+          });
+    } catch (err) {
+       console.log(err);
+    }
+     });
+ 
+ router.use("/products",async function(req,res)  {
+    try {
+       const [products,] = await  db.execute("select * from products");
+       res.render("products",{
+          urunler: products
+          });
+    } catch (err) {
+       console.log(err);
+    }
+     });
+  
+ router.use("/", async function(req,res)  {
+ 
+    try {
+       const [products,] = await  db.execute("select * from products");
+       res.render("index",{
+          urunler: products
+          });
+    } catch (err) {
+       console.log(err);
+    }});
+ 
+
+    module.exports = router;
